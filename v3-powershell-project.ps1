2d9e2336aa36694636c94f99529139a9e57ce774
BmiG <bowchung@googlemail.com>
2018-03-20 11:17:33 +0000

    Tidy
    Tidy Functions, add comment

--- v3-powershell-project.ps1
+++ v3-powershell-project.ps1
@@ -1,25 +1,46 @@
-﻿#####Return Installed Programs
+﻿<#
+DONE-System information, including OS version, machine build
+DONE-Latest windows updates and date installed
+DONE-Current connected devices, including model and make
+
+Power management settings for windows
+USB root hub power settings for USB power devices
+
+DONE-List of installed company applications, including versions and build number
+Confirm if the application is authenticated via single sign-on
+FQDN of the host server the application it should be currently connecting to
+Confirm if the machine can ping the application server
+Test the machine's connection to the database server
+
+Whilst I was re-writing this list a few more came up (oops) [10/03/2018]
+Confirm that the time on the machine is correct
+Confirm the application user cache setting
+Check if the required ports on firewall have been enabled
+#>
+
+#####Return Installed Programs
 function GetPrograms {
 
-param ($param)
-
-Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
-where {$_.DisplayName -like $param}
+    param ($param)
+    Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
+    where {$_.DisplayName -like $param}
 
 }
 
 $array = '.NET*','Terminal*', 'bighand*','dragon*'
 
 foreach ($item in $array){
-GetPrograms -param $item | SELECT DisplayName, Comments
-}
 
+    GetPrograms -param $item | SELECT DisplayName, Comments |
+    Format-Table –AutoSize
+
+}
 
 
 #####Retrieve current device information
 function GetCurrentDevice {
-
-Get-ItemProperty 'HKCU:\SOFTWARE\BigHand\BHRecorder\' | SELECT RecordingDeviceName, PlaybackDeviceName, PSPath, DeviceDriver, RecordingVolume, PlaybackVolume, DefaultLocalCodec
+    
+    Get-ItemProperty 'HKCU:\SOFTWARE\BigHand\BHRecorder\' | SELECT RecordingDeviceName, PlaybackDeviceName, PSPath, DeviceDriver, RecordingVolume, PlaybackVolume, DefaultLocalCodec
 
 }
 
@@ -31,23 +52,20 @@ GetCurrentDevice -list
 #searchable by required info
 #add - memory info
 function GetSystemInfo {
-
-Get-WmiObject -Class Win32_ComputerSystem
-
+    Get-WmiObject -Class Win32_ComputerSystem
 }
+
+GetSystemInfo Format-Table –AutoSize
+
+
 function GetUpdateInfo {
-
-Get-WmiObject -Class Win32_QuickFixEngineering
-
+    Get-WmiObject -Class Win32_QuickFixEngineering
 }
 
-GetSystemInfo -List
-GetUpdateInfo -Format-Table
+GetUpdateInfo Format-Table –AutoSize
 
 
 #####Device Manager and Power Management Settings
-
-
 
 
 Get-CimInstance Win32_OperatingSystem | Select-Object  Caption, InstallDate, ServicePackMajorVersion, OSArchitecture, BootDevice,  BuildNumber, CSName | FL
